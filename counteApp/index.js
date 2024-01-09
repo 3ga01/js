2a7c10e9a603ac4f5fad9b4f05f4f4ed4230ea65
user.name:"Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2024-01-09 14:13:06 +0000

    counter program done

--- counteApp/index.js
+++ counteApp/index.js
@@ -1,5 +1,23 @@
+const decreaseBtn = document.getElementById('decrementBtn');
+const increaseBtn = document.getElementById('incrementBtn');
+const resetBtn = document.getElementById('resetBtn');
 
-let age = window.prompt("How old are you?");
-age = parseInt(age);
-age += 1;
-console.log(age, typeof age);
+const countLabel = document.getElementById('countLabel');
+
+let count = 0;
+
+increaseBtn.onclick = () => {
+    count++;
+    countLabel.innerText = count;
+}
+
+decreaseBtn.onclick = () => {
+    count--;
+    countLabel.innerText = count;
+}
+
+resetBtn.onclick = () => {
+    count = 0;
+    countLabel.innerText = count;
+}
+
